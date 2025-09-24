82239103042b9d63bb127070459ca3049a6c7da9
lesja <lesjanikel@gitub.com>
2025-09-24 16:21:24 +0200

    fork added

--- main.cpp
+++ main.cpp
@@ -2,20 +2,26 @@
 #include <filesystem>
 #include <unistd.h>
 #include <vector>
+#include <sys/types.h>
+
 
 using namespace std;
 using namespace std::filesystem;
+
 void getParameters(bool &recursive, bool &case_sen, string &path, vector<string> &filenames, int, char*[]);
+void splitProcess(pid_t pid, vector<string> &filenames);
+
+
 int main(int argc, char* argv[])
 {
     bool recursive = false;
     bool case_sen = false;
     string path;
     vector<string> filenames;
+    pid_t pid = fork();
     // List files in default direc
     getParameters(recursive, case_sen, path, filenames, argc, argv);
-
-
+    splitProcess(pid, filenames);
 
     return 0;
 }
@@ -42,6 +48,29 @@ void getParameters(bool &recursive, bool &case_sen, string &path, vector<string>
             }
         }
 
-
 }
+
+//define child and parent processes
+void splitProcess(pid_t pid, vector<string> &filenames)
+{
+    switch(pid){
+        case -1: //error
+            cout << "Failed to fork" << endl;
+            exit(1);
+        case 0: //child
+        //searchFile();
+            break;
+
+        default: //parent
+            sleep(1); //todo: wait for child process
+            path dpath = current_path();
+            cout << dpath << endl;
+            for(const auto &entry:
+                    directory_iterator(dpath)){
+                    cout << "File: " << entry.path() << endl;
+            break;
+            }
+    }
+
+};
 
